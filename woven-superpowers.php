7cc31bd850b14d1bde3c0b2714b7590809002f60
WovenSocial <Hello@wovensocial.nz>
2025-11-27 13:24:18 +1300

    Fix GitHub repo constants in plugin header

--- woven-superpowers.php
+++ woven-superpowers.php
@@ -1,22 +1,26 @@
 <?php
-/**
+/*
  * Plugin Name: Woven Superpowers
+ * Plugin URI: https://github.com/bijmin/woven-superpowers
  * Description: Extends Voxel with new filtering and timeline features.
- * Version: 0.1
+ * Version: 1.0.0
  * Author: Woven Social
+ * Author URI: https://wovensocial.nz
  */
 
 if ( ! defined( 'ABSPATH' ) ) exit;
 
 define( 'WOVEN_SUPERPOWERS_PATH', plugin_dir_path( __FILE__ ) );
-defined( 'WSP_GITHUB_REPO' ) || define( 'WSP_GITHUB_REPO', 'your-account/woven-superpowers' );
+
+// GitHub auto-updater config
+defined( 'WSP_GITHUB_REPO' ) || define( 'WSP_GITHUB_REPO', 'bijmin/woven-superpowers' );
 defined( 'WSP_GITHUB_BRANCH' ) || define( 'WSP_GITHUB_BRANCH', 'main' );
 
 require_once __DIR__ . '/includes/class-github-updater.php';
 require_once __DIR__ . '/includes/class-timeline-filter.php';
 
 if ( is_admin() ) {
-	new \Woven\Superpowers\GitHub_Updater( __FILE__, WSP_GITHUB_REPO, WSP_GITHUB_BRANCH );
+    new \Woven\Superpowers\GitHub_Updater( __FILE__, WSP_GITHUB_REPO, WSP_GITHUB_BRANCH );
 }
 
 add_action( 'init', function() {
